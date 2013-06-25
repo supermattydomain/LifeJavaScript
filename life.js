0a4e9a5b3531cde56347ace9d21b58af1e2f374f
Matt <matt@supermatty.com>
2013-06-25 04:18:14 +0000

    Combine scripts.js into life.js. Show stability message using jquery toastmessage plugin. Move all javascript includes to end of html.
    git-svn-id: https://svn/svn/LifeJavaScript@2929 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- life.js
+++ life.js
@@ -143,7 +143,7 @@ $.extend(Life.Board.prototype, {
 			this.current = this.next;
 			this.next = temp;
 		} else {
-			// debug('Stable');
+			$().toastmessage('showNoticeToast', 'Stable');
 		}
 		return changed;
 	},
@@ -304,3 +304,46 @@ $.extend(Life, {
 		]
 	}
 });
+
+(function($) {
+	$(function() {
+		var buttonSingleStep = $('#buttonSingleStep'),
+			buttonStart = $('#buttonStart'),
+			buttonClear = $('#buttonClear'),
+			buttonRandomise = $('#buttonRandomise'),
+			checkboxWrap = $('#checkboxWrap'),
+			boardTable = $('table.LifeBoard'),
+			speedSlider = $('#speedSlider'),
+			board = new Life.Board(boardTable);
+		$('button, input[type="button"], input[type="checkbox"]').button();
+		checkboxWrap.on('click', function() {
+			board.toggleWrap();
+		});
+		buttonSingleStep.on('click', function() {
+			board.tick();
+		});
+		buttonStart.on('click', function() {
+			board.toggleRunning();
+		});
+		boardTable.on(Life.eventNames.started, function() {
+			buttonStart.val('Pause');
+		}).on(Life.eventNames.stopped, function() {
+			buttonStart.val('Resume');
+		});
+		buttonClear.on('click', function() {
+			board.clear();
+		});
+		buttonRandomise.on('click', function() {
+			board.randomise();
+		});
+		boardTable.find('td').on('click', function() {
+			board.handleClick(this.parentNode.rowIndex, this.cellIndex);
+		});
+		function updateDelay() {
+			board.setDelay(speedSlider.slider("option", "max") - speedSlider.slider("option", "value"));
+		}
+		speedSlider.slider({ min: 0, max: 1000, value: 500, stop: updateDelay });
+		updateDelay();
+		board.cannedShape(0, 0, 'glider gun');
+	});
+})(jQuery);
